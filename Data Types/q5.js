ae876b023c77787805e2d21d11ebd9fc76db2be4
Srikaran <srikaran2230@gmail.com>
2023-11-20 16:42:49 +0530

    Js

---
+++ Data Types/q5.js
@@ -0,0 +1,8 @@
+let length = prompt("Enter length");
+let breadth = prompt("Enter breadth");
+
+let perimeter = 2 * (length + breadth);
+console.log(`Perimeter of rectangle: ${perimeter} mts`);
+
+let area = length * breadth;
+console.log(`Area of rectangle: ${area} sqmts`);
